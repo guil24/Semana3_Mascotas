2b7fe83a0f10f5c379f3c41230d7ea6814fa04f4
guil24 <guillermo.maestre@hotmail.com>
2020-10-26 19:50:11 -0300

    master

---
+++ app/src/main/java/com/gmaestre/curso3_mascotas/MejoresMascotas.java
@@ -0,0 +1,50 @@
+package com.gmaestre.curso3_mascotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MejoresMascotas extends AppCompatActivity {
+
+    RecyclerView recyclerViewMascota;
+    RecyclerViewAdapter adapterMascota;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingAddButton);
+        fab.hide();
+
+        Toolbar myToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+
+        //boton para ir a activity anterior
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerViewMascota = findViewById(R.id.rvMascota);
+        recyclerViewMascota.setLayoutManager(new LinearLayoutManager(this));
+
+        adapterMascota = new RecyclerViewAdapter(obtenerMascota());
+        recyclerViewMascota.setAdapter(adapterMascota);
+    }
+
+    public List<ModeloMascota> obtenerMascota() {
+        List<ModeloMascota> mascota = new ArrayList<>();
+        mascota.add(new ModeloMascota("Max", R.drawable.dog));
+        mascota.add(new ModeloMascota("Charlie", R.drawable.cat));
+        mascota.add(new ModeloMascota("Luna", R.drawable.dog));
+        mascota.add(new ModeloMascota("Jack", R.drawable.cat));
+        mascota.add(new ModeloMascota("Milo", R.drawable.dog));
+        mascota.add(new ModeloMascota("Bailey", R.drawable.cat));
+        return mascota;
+    }
+}
